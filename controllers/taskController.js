4003767da85f57f629e4da24cb8eca2c98f11c0f
Diego pinzon <diegopinzondev@gmail.com>
2022-03-19 12:56:12 -0500

    delete task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -40,8 +40,55 @@ const getTask = async (req, res) => {
 
     res.json(task);
 };
-const updateTask = async (req, res) => {};
-const deleteTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+    const {id} = req.params;
+
+    const task = await Task.findById(id).populate('project');
+
+    if(!task){
+        const error = new Error(`Task not found`);
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error(`Action not allow`);
+        return res.status(403).json({msg: error.message})
+    }
+
+    task.name = req.body.name || task.name;
+    task.description = req.body.description || task.description;
+    task.priority = req.body.priority || task.priority;
+    task.dateDelivery = req.body.dateDelivery || task.dateDelivery;
+
+    try {
+        const taskStored = await task.save();
+        res.json(taskStored);
+    } catch (error) {
+        console.log(error)
+    }
+};
+const deleteTask = async (req, res) => {
+    const {id} = req.params;
+
+    const task = await Task.findById(id).populate('project');
+
+    if(!task){
+        const error = new Error(`Task not found`);
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error(`Action not allow`);
+        return res.status(403).json({msg: error.message})
+    }
+
+    try {
+        await task.deleteOne()
+        res.json({msg: `Task was eliminated`})
+    } catch (error) {
+        console.log(error)
+    }
+};
 const changeState = async (req, res) => {};
 
 
