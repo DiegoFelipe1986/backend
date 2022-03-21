f86ff418d2854590b4d2c2eec960859bd8d46cd9
Diego pinzon <diegopinzondev@gmail.com>
2022-03-21 10:28:05 -0500

    fix project model add array

--- models/Project.js
+++ models/Project.js
@@ -24,6 +24,12 @@ const projectsSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    tasks:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Task'
+        }
+    ],
     collaborator:[
         {
             type: mongoose.Schema.Types.ObjectId,
